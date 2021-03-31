6f98cd4cfff38ed7cc07d6dc3c02020f340beb21
Alexander Neville <alexander.neville@icloud.com>
2021-03-31 14:21:50 +0100

    automated backup

---
+++ .doom.d/config.el
@@ -0,0 +1,97 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "Alexander Neville"
+      user-mail-address "alexander.neville@icloud.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+(setq
+
+ doom-font (font-spec :family "Roboto Mono" :size 17 :weight 'regular)
+ doom-variable-pitch-font (font-spec :family "Roboto" :size 17)
+ doom-theme 'doom-one
+ org-directory "~/code/org/"
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+ display-line-numbers-type 'relative
+ auto-save-default nil
+ make-backup-files nil
+ projectile-project-search-path '("~/code")
+ projectile-switch-project-action #'projectile-dired
+
+ mouse-wheel-scroll-amount '(3 ((shift) . 1)) ;; one line at a time
+ mouse-wheel-progressive-speed nil ;; don't accelerate scrolling
+ mouse-wheel-follow-mouse 't ;; scroll window under mouse
+ doom-modeline-height 35
+ doom-modeline-enable-word-count nil
+ doom-modeline-buffer-encoding nil
+ org-cycle-separator-lines 2
+
+)
+
+(map! :leader
+      :desc "switch buffer"
+      "b s" #'counsel-ibuffer)
+
+
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+(defun alex/org-mode-setup ()
+  (org-indent-mode)
+  (variable-pitch-mode 1) ;; If you want fancy variable width fonts.
+  (visual-line-mode 1))
+
+(defun alex/org-font-setup ()
+  (font-lock-add-keywords 'org-mode
+                          '(("^ *\\([-]\\) "
+                             (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+
+  (set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-table nil   :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch))
+
+(use-package org
+  :hook (org-mode . alex/org-mode-setup)
+  :config
+  ;(setq org-ellipsis " ")
+  ;;(setq org-ellipsis " ⤵")
+  (setq org-indent-indentation-per-level 2)
+  (setq org-hide-emphasis-markers t)
+  (setq org-agenda-files '("~/code/org/agenda.org"))
+  (alex/org-font-setup))
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
