3ca062e66b4dba9b00e78a79adebcc55539c6df4
Alexander Neville <alexander.neville@icloud.com>
2021-06-02 19:10:25 +0100

    automated backup

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -21,6 +21,9 @@ require("awful.hotkeys_popup.keys")
 
 -- source parts of my config
 require("core.error_handling")
+require("core.layouts")
+local menu_components = require("components.menus")
+
 local variables = require("core.variables")
 local modkey = variables.modkey
 
@@ -28,27 +31,6 @@ local modkey = variables.modkey
 beautiful.init("/home/alex/code/dotfiles/.config/awesome/themes/default.lua")
 
 
--- Table of layouts to cover with awful.layout.inc, order matters.
-awful.layout.layouts = {
-    -- awful.layout.suit.floating,
-    awful.layout.suit.tile,
-    -- awful.layout.suit.tile.left,
-    -- awful.layout.suit.tile.bottom,
-    -- awful.layout.suit.tile.top,
-    -- awful.layout.suit.fair,
-    -- awful.layout.suit.fair.horizontal,
-    -- awful.layout.suit.spiral,
-    -- awful.layout.suit.spiral.dwindle,
-    awful.layout.suit.max,
-    -- awful.layout.suit.max.fullscreen,
-    -- awful.layout.suit.magnifier,
-    -- awful.layout.suit.corner.nw,
-    -- awful.layout.suit.corner.ne,
-    -- awful.layout.suit.corner.sw,
-    -- awful.layout.suit.corner.se,
-}
-
-local menu_components = require("components.menus")
 
 
 menubar.utils.terminal = variables.terminal
